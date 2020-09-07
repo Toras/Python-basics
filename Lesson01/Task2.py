7e9f90078c148b0ea7c6386435fe64dca692b6ac
Toras <efpan@mail.ru>
2020-09-07 21:21:42 +0700

    Delete Task2.py

--- Lesson01/Task2.py
+++
@@ -1,8 +0,0 @@
-TimeInSec = int(input("Введите время в секундах: "))
-tis = int(TimeInSec)
-h = TimeInSec // 3600
-m = (TimeInSec - h * 3600) // 60
-s = TimeInSec - (h * 3600 + m * 60)
-
-print("{0} секунд = {1:02d}:{2:02d}:{3:02d}".format(TimeInSec, h, m, s))
-print(f"{TimeInSec:d} секунд = {h:02d}:{m:02d}:{s:02d}")
